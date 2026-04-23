4dd0168d9c35f63e3c2fda90e6f023f96fb3c8cb
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-23 19:25:32 -0300

    feat: initialize wrapper dependency definitions and update gitignore paths

---
+++ wrappers/deps/fdeclare.deps.h
@@ -0,0 +1,3 @@
+#include "../../dependencies/CArgvParse.h"
+#include "../../dependencies/CWebStudio.h"
+#include "../../dependencies/BearHttpsClient.h"
---
+++ wrappers/deps/fdefine.deps.c
@@ -0,0 +1,7 @@
+//silver_chain_scope_start
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
+#include "../imports/imports.fdeclare.h"
+//silver_chain_scope_end
+#include "../../dependencies/CArgvParseOne.c"
+#include "../../dependencies/CWebStudioOne.c"
+#include "../../dependencies/BearHttpsClientOne.c"
