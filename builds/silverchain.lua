96d19878cac6cebc4070e0f34301f3a64897feae
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-23 17:35:06 -0300

    feat: initialize Horizon project configuration and SilverChain build infrastructure

---
+++ builds/silverchain.lua
@@ -0,0 +1,31 @@
+function silver_chain_organize()
+
+
+
+    darwin.silverchain.generate({
+        src = "src",
+        project_short_cut = PROJECT_NAME,
+        tags = { 
+------------------DECLARATIONS
+            "typedef",
+            "fdeclare", 
+------------------IMPLEMENTATIONS
+            "struct", 
+            "fdefine"
+
+    }})
+    
+end 
+
+darwin.add_recipe({
+    inputs={
+        "src",
+        "dependencies"
+    },
+    outs= {
+        "src/fdeclare.all.h"
+    },
+    name="silver_chain_organize",
+    description = "Organize source files using SilverChain",
+    callback = silver_chain_organize
+})
---
+++ darwinconf.lua
@@ -0,0 +1,15 @@
+PROJECT_NAME = "horizon"
+CONTANIZER   = darwin.argv.get_flag_arg_by_index({ "provider", "contanizer" }, 1, "docker")
+VERSION      = "0.0.0"
+LICENSE      = "MIT"
+URL          = "https://github.com/OUIsolutions/Horizon"
+DESCRIPITION = "A database with horizontal scalability"
+FULLNAME     = "OUI Solutions"
+EMAIL        = "mateusmoutinho01@gmail.com"
+SUMARY       = ""
+LAUNGUAGE    = "c"
+
+
+
+-- Load all build recipes from builds/ directory
+darwin.load_all("builds")
