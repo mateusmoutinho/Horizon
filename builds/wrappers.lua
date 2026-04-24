0e98964ed5cfbd636d301be98d558720716382b5
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-24 00:30:58 -0300

    feat: add release directory and implement private import definition in wrapper build scripts

--- builds/wrappers.lua
+++ builds/wrappers.lua
@@ -1,24 +1,21 @@
 function build_wrapper(wrapper_name)
-local wrapper_source = darwin.dtw.load_file("wrappers/" .. wrapper_name .. ".c")
-darwin.dtw.write_file("release/" .. wrapper_name .. "_pure.c", wrapper_source)
 
 
-local lib_declare = darwin.dtw.load_file("dependencies/" .. wrapper_name .. ".h")
-local wrapper_with_header = wrapper_source ..  lib_declare
-darwin.dtw.write_file("release/" .. wrapper_name .. "with_headers.h", wrapper_with_header)
+    local hide_import = "#define PRIVATE_HORIZON_HIDE_IMPORT"
+
+    local wrapper_source = darwin.dtw.load_file("wrappers/" .. wrapper_name .. ".c")
+    wrapper_source = hide_import.."\n"..wrapper_source
+    darwin.dtw.write_file("release/" .. wrapper_name .. "_pure.c",wrapper_source)
 
 
-local lib_one = darwin.dtw.load_file("dependencies/" .. wrapper_name .. "one.c")
-local wrapper_one = wrapper_source .. lib_one
-darwin.dtw.write_file("release/" .. wrapper_name .. "one.c", wrapper_one)
+    local lib_declare = darwin.dtw.load_file("dependencies/" .. wrapper_name .. ".h")
+    local wrapper_with_header = lib_declare.. "\n".. wrapper_source 
+    darwin.dtw.write_file("release/" .. wrapper_name .. "_with_headers.h", wrapper_with_header)
 
 
-
-
-
-
-
-
+    local lib_one = darwin.dtw.load_file("dependencies/" .. wrapper_name .. "one.c")
+    local wrapper_one = lib_one.. "\n".. wrapper_source 
+    darwin.dtw.write_file("release/" .. wrapper_name .. "_one.c", wrapper_one)
 
 end
 
