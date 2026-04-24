2bec4515b8a6d1e35fc868a6c1cf746c84be82c4
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-24 03:26:44 -0300

    feat: add header-inclusive and full amalgamation variants for wrappers and include std library

--- builds/wrappers.lua
+++ builds/wrappers.lua
@@ -6,7 +6,21 @@ function Build_wrapper(wrapper_name)
         "wrappers/" .. wrapper_name .. ".c",
         function(import, path) return 'dont-include' end
     )
+    local with_headders = darwin.camalgamator.generate_amalgamation_with_callback(
+        "wrappers/" .. wrapper_name .. ".c",
+        function(import, path)
+            if darwin.dtw.ends_with(path,".h") then
+                return 'include-once' 
+            end
+            return 'dont-include'
+         end
+    )
+    local full = darwin.camalgamator.generate_amalgamation(
+        "wrappers/" .. wrapper_name .. ".c"
+    )
     darwin.dtw.write_file("release/" .. wrapper_name .. "_pure.c", pure)
+    darwin.dtw.write_file("release/" .. wrapper_name .. "_with_headers.c", with_headders)
+    darwin.dtw.write_file("release/" .. wrapper_name .. "_full.c", full)
 end
 
 
@@ -18,8 +32,10 @@ function Wrappers()
     Build_wrapper("BearHttpsClient")
     Build_wrapper("CArgvParse")
     Build_wrapper("CWebStudio")
+    Build_wrapper("std")
 
-    
+    --std full is equal to std with_headers so we remove it
+    darwin.dtw.remove_any("release/std_full.c")
 
 end
 
