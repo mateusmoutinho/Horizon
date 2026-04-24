7eb0afc0a32aa33913bd61e0ac0486cdd5e07df6
matesmoutinho <matesmoutinho01@gmail.com>
2026-04-24 03:04:29 -0300

    refactor: replace custom build script logic with camalgamator and update wrapper generation process

--- builds/wrappers.lua
+++ builds/wrappers.lua
@@ -1,31 +1,24 @@
-function build_wrapper(wrapper_name)
 
+function Build_wrapper(wrapper_name)
+    
 
-    local hide_import = "#define PRIVATE_HORIZON_HIDE_IMPORTS"
-
-    local wrapper_source = darwin.dtw.load_file("wrappers/" .. wrapper_name .. ".c")
-    wrapper_source = hide_import.."\n"..wrapper_source
-    darwin.dtw.write_file("release/" .. wrapper_name .. "_pure.c",wrapper_source)
-
-
-    local lib_declare = darwin.dtw.load_file("dependencies/" .. wrapper_name .. ".h")
-    local wrapper_with_header = lib_declare.. "\n".. wrapper_source 
-    darwin.dtw.write_file("release/" .. wrapper_name .. "_with_headers.h", wrapper_with_header)
-
-
-    local lib_one = darwin.dtw.load_file("dependencies/" .. wrapper_name .. "one.c")
-    local wrapper_one = lib_one.. "\n".. wrapper_source 
-    darwin.dtw.write_file("release/" .. wrapper_name .. "_one.c", wrapper_one)
-
+    local pure = darwin.camalgamator.generate_amalgamation_with_callback(
+        "wrappers/" .. wrapper_name .. ".c",
+        function(import, path) return 'dont-include' end
+    )
+    darwin.dtw.write_file("release/" .. wrapper_name .. "_pure.c", pure)
 end
 
-function wrappers()
 
 
-    build_wrapper("BearHttpsClient")
-    build_wrapper("CArgvParse")
-    build_wrapper("CWebStudio")
-    darwin.dtw.copy_any_overwriting("wrapers/std.c", "release/std.c")
+
+function Wrappers()
+
+
+    Build_wrapper("BearHttpsClient")
+    Build_wrapper("CArgvParse")
+    Build_wrapper("CWebStudio")
+
     
 
 end
@@ -39,5 +32,5 @@ darwin.add_recipe({
     outs = {},
     name = "wrappers",
     description = "Wrapper source files",
-    callback = wrappers
+    callback = Wrappers
 })
--- wrappers/BearHttpsClient.c
+++ wrappers/BearHttpsClient.c
@@ -1,1 +1,4 @@
 #include "../dependencies/BearHttpsClient.h"
+
+
+//aftger
